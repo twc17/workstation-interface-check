2ce1511714b2a84fa3b89e0db9bb9a686f3b1726
Troy <twc17@pitt.edu>
2017-11-08 08:04:37 -0500

    Readability. Still working on functionality

--- modify_interfaces.py
+++ modify_interfaces.py
@@ -19,9 +19,10 @@ Options:
 # Troy <twc17@pitt.edu>
 
 # Last modified
-# 11/04/2017
+# 11/08/2017
 
-# TODO: Lookup docopt info, add a -p argument for pretent mode
+# TODO: Lookup docopt info, add a -p argument for pretend mode
+# TODO: try/catch opening files correctly
 
 # External libs
 import os
@@ -31,8 +32,27 @@ import docopt
 import netmiko
 import datetime
 
-# Log file in current dir: modify_interfaes_DATETIME.log
-LOG_FILE = 'modify_interfaces_{}.log'.format(datetime.datetime.now().strftime('%Y-%m-%d'))
+#################################################################################
+
+# Command line arguments
+arguments = docopt.docopt(__doc__, version='modify_interfaces.py version 1.0.0')
+
+SWITCH_NAME = arguments['<switch_name>']
+
+INTERFACE_CONFIG = []
+# Open file containing interface configurations and add them to INTERFACE_CONFIG
+with open(arguments['<interface_config>'], 'r') as f:
+    INTERFACE_CONFIG.append(f.readline())
+
+INTERFACE_LIST = []
+# Open file of interfaces and add them to the INTERFACE_LIST
+with open(arguments['<interface_list>'], 'r') as f:
+    INTERFACE_LIST.append(f.readline().strip())
+
+# Log file in current dir: modify_interfaes_SWITCH.log
+LOG_FILE = 'modify_interfaces_{}.log'.format(SWITCH_NAME)
+
+#################################################################################
 
 def write_log(entry):
     """Write an entry to the log file"""
@@ -56,4 +76,32 @@ def check_host(host):
     except:
         return False
 
+def get_interface_config(interface, ssh):
+    """Get the running config for a specific interface
 
+    Arguments:
+        interface -- the interface to get the config for
+        ssh -- connection to the switch
+
+    Returns:
+        config -- list of configs on the interface
+    """
+
+    cmd = 'sh run int {}'.format(interface)
+
+    result = ssh.find_prompt() + "\n"
+    result += ssh.send_command_expect(cmd)
+    config = result.split('\n')
+
+    return config
+
+#################################################################################
+
+def main():
+    pass
+
+#################################################################################
+
+# Run the program
+if __name__ == "__main__":
+    main()
